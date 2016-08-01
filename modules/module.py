ef36d6618594ab08b702c82dbaeac8bc8e710a5b
willardshikami <willardshikami@gmail.com>
2016-08-01 07:33:18 +0300

    updated assignment

---
+++ modules/module.py
@@ -0,0 +1,6 @@
+def enter_num():
+    number = raw_input("Enter a positive number: ")
+    return number
+
+print enter_num()
+
---
+++ modules/tribonacci.py
@@ -0,0 +1,12 @@
+from module import enter_num 
+
+
+def tribo(n):
+    n = enter_num
+    queue = [1, 1, 1]
+    for i in xrange(n - 2):
+        queue = queue[1:] + [sum(queue)]
+    return queue[-1]
+
+
+print ("Tribonacci for 4 is {}".format(tribo(4)))
